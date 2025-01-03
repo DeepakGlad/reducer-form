654acc7f14337f0e95ce8dd56547792d9b503fd9
Deepak Gladi <deepak.happ@gmail.com>
2025-01-03 10:31:27 +0530

    dded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useReducer } from 'react'
 
+import { useRef } from 'react'
+
 import './App.css'
 
 function App() {
+const username = useRef("");
 
+
+
+const initialState = {
+  username: "",
+  email: "",
+  firstname:"",
+  lastname:"",
+}
 
   const reducer =(state,action)=>{
     switch(action.type){
@@ -21,6 +32,9 @@ function App() {
 
             case 'submit':
               return {submit:action.payload}
+
+              case 'reset':
+                return {reset:initialState}
     }
   }
 
@@ -52,19 +66,27 @@ function App() {
     event.preventDefault();
     dispach({type:"submit",payload:state})
     console.log("submit")
+
+    const form = username.current
+    alert(`${form['username'].value} ${form['lastname'].value}`)
+  }
+
+  const onClickReset = () =>{
+    dispach({type:"reset", payload:"" });
+    username="";
   }
 
   return (
     <>
       <div>
-        <form>
+        <form ref={username}>
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
         <div class="sm:col-span-4">
           <label for="username" class="block text-sm/6 font-medium text-gray-900">Username</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
             
-              <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="janesmith" value={state.username} onChange={handlechange} />
+              <input  type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" placeholder="janesmith" value={state.username} onChange={handlechange} />
             </div>
           </div>
         </div>
@@ -103,7 +125,7 @@ function App() {
         </div>
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
-              <button type="button" class="text-sm/6 font-semibold text-gray-900" >Reset</button>
+              <button type="button" class="text-sm/6 font-semibold text-gray-900" onClick={onClickReset} >Reset</button>
               <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={onClickHandle}>Save</button>
           </div>
         </form>
